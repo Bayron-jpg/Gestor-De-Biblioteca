87ab750ab4ea156fa2d7310d9fb37d2a201a3829
Bayron <bayron221906@gmail.com>
2026-06-19 16:39:50 -0400

    Creación de bases de datos locales

---
+++ init-db.sql
@@ -0,0 +1,39 @@
+-- 1. Autores --
+DROP DATABASE IF EXISTS db_autores_dev;
+CREATE DATABASE db_autores_dev;
+
+-- 2. Géneros --
+DROP DATABASE IF EXISTS db_generos_dev;
+CREATE DATABASE db_generos_dev;
+
+-- 3. Libros --
+DROP DATABASE IF EXISTS db_libros_dev;
+CREATE DATABASE db_libros_dev;
+
+-- 4. Bibliotecarios --
+DROP DATABASE IF EXISTS db_bibliotecarios_dev;
+CREATE DATABASE db_bibliotecarios_dev;
+
+-- 5. Turnos --
+DROP DATABASE IF EXISTS db_turnos_dev;
+CREATE DATABASE db_turnos_dev;
+
+-- 6. Telefonos --
+DROP DATABASE IF EXISTS db_telefonos_dev;
+CREATE DATABASE db_telefonos_dev;
+
+-- 7. Direcciones --
+DROP DATABASE IF EXISTS db_direcciones_dev;
+CREATE DATABASE db_direcciones_dev;
+
+-- 8. Usuarios --
+DROP DATABASE IF EXISTS db_usuarios_dev;
+CREATE DATABASE db_usuarios_dev;
+
+-- 9. Pisos --
+DROP DATABASE IF EXISTS db_pisos_dev;
+CREATE DATABASE db_pisos_dev;
+
+-- 10. Salas --
+DROP DATABASE IF EXISTS db_salas_dev;
+CREATE DATABASE db_salas_dev;
